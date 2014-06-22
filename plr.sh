e36ca18045a19054d7223e2ea3b5178c113b5927
Ronert Obst <ronert.obst@gmail.com>
2014-06-22 20:41:53 +0200

    initial commit

---
+++ plr.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install PLR
+sshpass -p gpadmin ssh -o StrictHostKeyChecking=no gpadmin@phd1 'mkdir /vagrant/plr; cp /vagrant/plr*.tgz plr'
+sshpass -p gpadmin ssh -o StrictHostKeyChecking=no gpadmin@phd1 'tar -xzf /vagrant/plr*.tgz -C /vagrant/plr'
+sshpass -p gpadmin ssh -o StrictHostKeyChecking=no gpadmin@phd1 'source /usr/local/hawq/greenplum_path.sh; cd /vagrant/plr; /vagrant/plr/plr_install.sh -f /home/gpadmin/HAWQ_Segment_Hosts.txt'
+sshpass -p gpadmin ssh -o StrictHostKeyChecking=no gpadmin@phd1 'source /usr/local/hawq/greenplum_path.sh; export MASTER_DATA_DIRECTORY=/data1/master/gpseg-1/;  gpstop -ar'
