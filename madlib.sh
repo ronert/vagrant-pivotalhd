bd13e4bc18739c79c718e1cb86a200639e18689b
Ronert Obst <ronert.obst@gmail.com>
2014-08-05 16:54:15 +0200

    use new madlib version

--- madlib.sh
+++ madlib.sh
@@ -2,4 +2,4 @@
 
 # Install MADlib
 sshpass -p gpadmin ssh -o StrictHostKeyChecking=no gpadmin@phd1 'mkdir /vagrant/madlib; tar -xzf /vagrant/madlib*.tgz -C /vagrant/madlib'
-sshpass -p gpadmin ssh -o StrictHostKeyChecking=no gpadmin@phd1 'source /usr/local/hawq/greenplum_path.sh; cd /vagrant/madlib/madlib_1.5; /vagrant/madlib/madlib_1.5/hawq_install.sh -r /vagrant/madlib/madlib_1.5/madlib*.rpm  -f /home/gpadmin/HAWQ_Segment_Hosts.txt'
+sshpass -p gpadmin ssh -o StrictHostKeyChecking=no gpadmin@phd1 'source /usr/local/hawq/greenplum_path.sh; cd /vagrant/madlib/madlib_1.6; /vagrant/madlib/madlib_1.6/hawq_install.sh -r /vagrant/madlib/madlib_1.6/madlib*.rpm  -f /home/gpadmin/HAWQ_Segment_Hosts.txt'
